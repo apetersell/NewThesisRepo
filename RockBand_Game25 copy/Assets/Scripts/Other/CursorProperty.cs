36f4623ef6293f9dae93248eeedbf72c549a6102
Andrew Petersell <andrewpetersell@gmail.com>
2018-05-16 15:16:45 -0400

    New Cursor/Sounds

--- RockBand_Game25 copy/Assets/Scripts/Other/CursorProperty.cs
+++ RockBand_Game25 copy/Assets/Scripts/Other/CursorProperty.cs
@@ -6,24 +6,21 @@ using UnityEngine.UI;
 public class CursorProperty : MonoBehaviour {
 
 	public Texture2D usual;
-	public Sprite[] miniGameIcons;
-	public Vector2 cursorCenter;
+	public Vector2 hotSpot;
 
 	// Use this for initialization
 	void Start () 
 	{
-		//Cursor.SetCursor (usual, Vector2.zero, CursorMode.Auto);
+		Cursor.SetCursor (usual, hotSpot, CursorMode.Auto);
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		transform.position = Input.mousePosition;
 	}
 
 	public void changeCursor (int index)
 	{
-		//Cursor.SetCursor (miniGameIcons[index], Vector2.zero, CursorMode.Auto);	
-		GetComponent<Image>().sprite = miniGameIcons[index];
+		
 	}
 }
